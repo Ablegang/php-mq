bc9babcfd3c2a307a0e0f9078143c55ce596141a
object <2252390865@qq.com>
2018-12-21 17:59:32 +0800

    修改sql

--- queue.sql
+++ queue.sql
@@ -1,13 +1,13 @@
-CREATE TABLE `queues`(
-  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `tube` VARCHAR(30) NOT NULL DEFAULT 'default',
-  `status` ENUM('ready','reserved'),
-  `job_data` TEXT NOT NULL,
-  `attempts` INT UNSIGNED DEFAULT 0,
-  `sort` INT UNSIGNED DEFAULT 0,
-  `reserved_at` INT(10) UNSIGNED DEFAULT NULL,
-  `available_at` INT(10) UNSIGNED NOT NULL,
-  `created_at` INT(10) UNSIGNED NOT NULL，
+CREATE TABLE `queues` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `tube` varchar(30) NOT NULL DEFAULT 'default',
+  `status` enum('ready','reserved') DEFAULT 'ready',
+  `job_data` text NOT NULL,
+  `attempts` int(11) NOT NULL DEFAULT '0',
+  `sort` int(10) NOT NULL DEFAULT '100',
+  `reserved_at` int(11) DEFAULT NULL,
+  `available_at` int(11) DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `queues_index`(`tube`)
-) ENGINE=InnoDB charset=utf8;
+  KEY `queues_index` (`tube`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
